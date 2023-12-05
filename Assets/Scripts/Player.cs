a0bccf8ee1a2d716f272496748e55d332b37a023
mitsuda_yumemi <yumemitsuglf.471.9@gmail.com>
2023-12-05 17:59:07 +0900

    プレイヤーに衝突判定を追加

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Player : MonoBehaviour
 {
@@ -8,15 +8,51 @@ public class Player : MonoBehaviour
     private bool isWalking = false;
     public bool IsWalking => isWalking;
 
+    private float playerHeight = 2f;
+    private float playerRadius = 0.7f;
+
     private void Update()
     {
+        float moveDistance = Time.deltaTime * moveSpeed;
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
-
-        isWalking = inputVector != Vector2.zero;
-
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
 
-        transform.position += moveDir * Time.deltaTime * moveSpeed;
+        // 進行方向には進めない場合
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+
+        if (!canMove)
+        {
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+
+            // X方向での回転だけはできるかチェック
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+
+            if(canMove)
+            {
+                moveDir = moveDirX;
+            }
+            else
+            {
+                // zだけならいけるか確認
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+
+                // X方向での回転だけはできるかチェック
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+
+                if (canMove)
+                {
+                    moveDir = moveDirZ;
+                }
+            }
+        }
+
+        if (!canMove)
+        {
+            return;
+        }
+
+        isWalking = inputVector != Vector2.zero;
+        transform.position += moveDir * moveDistance;
 
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * 10);
     }
