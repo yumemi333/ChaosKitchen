a4cb461fc35979291e6de448c4958076289daef3
mitsuda_yumemi <yumemitsuglf.471.9@gmail.com>
2023-12-05 15:09:04 +0900

    プレイヤーの移動とRotateを設定

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 7f;
+
+    private void Update()
+    {
+        Vector2 inputVector = new Vector2(0, 0);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y++;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y--;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x--;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x++;
+        }
+
+        inputVector = inputVector.normalized;
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+
+        transform.position += moveDir * Time.deltaTime * moveSpeed;
+
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * 10);
+    }
+}
