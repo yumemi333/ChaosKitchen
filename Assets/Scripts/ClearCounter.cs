e3bbfb0cf6dc9b0e0845aa1e00f25868708866c2
mitsuda_yumemi <yumemitsuglf.471.9@gmail.com>
2023-12-06 12:08:26 +0900

    カウンターの設置とInteractした際の処理を準備

---
+++ Assets/Scripts/ClearCounter.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearCounter : MonoBehaviour
+{
+    public void Interact()
+    {
+        Debug.Log("Hello");
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,14 +4,24 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private GameInput gameInput;
+    [SerializeField] private LayerMask counters;
 
     private bool isWalking = false;
     public bool IsWalking => isWalking;
 
     private float playerHeight = 2f;
+    private float interactDistance = 2f;
     private float playerRadius = 0.7f;
 
+
+    private Vector3 lastInteractDir;
     private void Update()
+    {
+        HandleMovement();
+        HandleInteraction();
+    }
+
+    private void HandleMovement()
     {
         float moveDistance = Time.deltaTime * moveSpeed;
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
@@ -27,7 +37,7 @@ public class Player : MonoBehaviour
             // X方向での回転だけはできるかチェック
             canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
 
-            if(canMove)
+            if (canMove)
             {
                 moveDir = moveDirX;
             }
@@ -55,5 +65,25 @@ public class Player : MonoBehaviour
         transform.position += moveDir * moveDistance;
 
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * 10);
+    }
+
+    private void HandleInteraction()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+
+        if (moveDir != Vector3.zero)
+        {
+            lastInteractDir = moveDir;
+        }
+
+        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, counters))
+        {                
+            // ClearCounterに当たった
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                clearCounter.Interact();
+            }
+        }
     }
 }
