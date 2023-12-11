ffbb036bec37103dce1fa1ec568c319b7303b006
mitsuda_yumemi <yumemitsuglf.471.9@gmail.com>
2023-12-11 16:22:02 +0900

    クリアカウンターにコンテナから取り出したキッチンオブジェクトを配置するように対応。クリアカウンターの選択時のハイライトをおこなうオブジェクトの参照外れ対応

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -5,6 +5,16 @@ public class ClearCounter : BaseCounter
 
     public override void Interact(Player player)
     {
+        if (HasKitchenObject())
+        {
 
+        }
+        else
+        {
+            if (player.HasKitchenObject())
+            {
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            }
+        }
     }
 }
