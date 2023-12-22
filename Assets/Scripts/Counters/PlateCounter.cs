fc1261b3b2687827fdd17efc456b309ace268c1a
mitsuda_yumemi <yumemitsuglf.471.9@gmail.com>
2023-12-22 16:54:26 +0900

    皿を生成するカウンターを作成

---
+++ Assets/Scripts/Counters/PlateCounter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+
+public class PlateCounter : BaseCounter
+{
+    private float spawnPlateTimer;
+    private float spawnPlateCount;
+
+    private const float spawnPlateTimerMax = 3;
+    private const float spawnPlateMax = 5;
+
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+
+    public EventHandler OnSpawnEvent;
+    public EventHandler OnPlateRemovedEvent;
+
+    private void Update()
+    {
+        if (spawnPlateCount < spawnPlateMax)
+        {
+            spawnPlateTimer += Time.deltaTime;
+            if (spawnPlateTimer > spawnPlateTimerMax)
+            {
+                OnSpawnEvent?.Invoke(this, EventArgs.Empty);
+                spawnPlateTimer = 0;
+                spawnPlateCount++;
+            }
+        }
+
+
+    }
+
+    public override void Interact(Player player)
+    {
+        if (!player.HasKitchenObject())
+        {
+            if (spawnPlateCount > 0)
+            {
+                spawnPlateCount--;
+                KitchenObject.SpawnKithenObject(kitchenObjectSO, player);
+                OnPlateRemovedEvent?.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Counters/PlateCounterVisual.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlateCounterVisual : MonoBehaviour
+{
+    [SerializeField] private PlateCounter plateCounter;
+    [SerializeField] private Transform counterTop;
+    [SerializeField] private Transform plateVisualPrefab;
+
+    private Stack<GameObject> plateVisualGameObjectList = new Stack<GameObject>();
+
+    public void Start()
+    {
+        plateCounter.OnSpawnEvent += PlatesCounter_OnPlateSpawned;
+        plateCounter.OnPlateRemovedEvent += PlatesCounter_OnPlateRemoved;
+    }
+
+    private void PlatesCounter_OnPlateRemoved(object sender, EventArgs e)
+    {
+       var prefab =  plateVisualGameObjectList.Pop();
+        Destroy(prefab);
+    }
+
+    private void PlatesCounter_OnPlateSpawned(object sender, System.EventArgs e)
+    {
+        Transform plateVisualTransform = Instantiate(plateVisualPrefab, counterTop);
+        float plateOffsetY = 0.1f;
+        plateVisualTransform.localPosition = new Vector3(0, plateOffsetY * plateVisualGameObjectList.Count, 0);
+        plateVisualGameObjectList.Push(plateVisualTransform.gameObject);
+    }
+
+}
