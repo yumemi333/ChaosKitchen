204df104cd8e9c074b0fdcf068e8a7ab3c631761
mitsuda_yumemi <yumemitsuglf.471.9@gmail.com>
2023-12-25 12:12:19 +0900

    皿の上で料理を完成させるように設定

---
+++ Assets/Scripts/PlateCompleteVisual.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using static PlateKitchenObject;
+
+public class PlateCompleteVisual : MonoBehaviour
+{
+    [Serializable]
+    public class KitchenObjectSO_GameObject
+    {
+        public KitchenObjectSO kitchenObjectSO;
+        public GameObject gameObject;
+    }
+
+    [SerializeField] private PlateKitchenObject plateKitchenObject;
+    [SerializeField] private List<KitchenObjectSO_GameObject> kitchenObjectSO_GameObjects;
+
+    void Start()
+    {
+        plateKitchenObject.OnIngredientAdded += PlateKitchenObject_OnIngredientAdded;
+        foreach (var item in kitchenObjectSO_GameObjects)
+        {
+            item.gameObject.SetActive(false);
+        }
+    }
+
+    private void PlateKitchenObject_OnIngredientAdded(object sender, OnIngredientAddedEventArgs e)
+    {
+        var ingredient = kitchenObjectSO_GameObjects.FirstOrDefault(a => a.kitchenObjectSO.Name == e.Ingredient.Name);
+        if (ingredient != null)
+        {
+            ingredient.gameObject.SetActive(true);
+        }
+    }
+
+}
--- Assets/Scripts/PlateKitchenObject.cs
+++ Assets/Scripts/PlateKitchenObject.cs
@@ -1,23 +1,37 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
 public class PlateKitchenObject : KitchenObject
 {
+    public event EventHandler<OnIngredientAddedEventArgs> OnIngredientAdded;
+    public class OnIngredientAddedEventArgs: EventArgs
+    {
+        public KitchenObjectSO Ingredient;
+    }
+
     private List<KitchenObjectSO> kitchenObjects = new List<KitchenObjectSO>();
     [SerializeField] private List<KitchenObjectSO> validKitchenObject = new List<KitchenObjectSO>();
 
     public bool TryAddIngredient(KitchenObjectSO kitchenObjectSO)
     {
+        // もう所持している
         if (kitchenObjects.Any(e=> e.name == kitchenObjectSO.name))
         {
             return false;
         }
+
+        // 正しいキッチンオブジェクト
         if(!validKitchenObject.Any(e=> e.name == kitchenObjectSO.name))
         {
             return false;
         }
+        
         kitchenObjects.Add(kitchenObjectSO);
+
+        OnIngredientAdded?.Invoke(this, new OnIngredientAddedEventArgs { Ingredient = kitchenObjectSO});
+
         return true;
     }
 }
