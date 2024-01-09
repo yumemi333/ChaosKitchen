7e4084e3385e3e3ea2a1604a1552774625e5c884
mitsuda_yumemi <yumemitsuglf.471.9@gmail.com>
2024-01-09 13:57:03 +0900

    Mainシーンを用意

---
+++ Assets/Scripts/MainManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainManager : MonoBehaviour
+{
+    public static MainManager Instance { get; private set; }
+    PlayerInputActions inputActions;
+
+    private void Awake()
+    {
+        Instance = this;
+        DontDestroyOnLoad(this);
+    }
+
+    private void Update()
+    {
+    }
+}
